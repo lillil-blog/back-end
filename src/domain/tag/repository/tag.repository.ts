54475597a3ec14fa21c6c994252f536359cf18e4
오성재 <monaco970808@gmail.com>
2024-04-02 15:24:12 +0900

    add: 주석 추가

--- src/domain/tag/repository/tag.repository.ts
+++ src/domain/tag/repository/tag.repository.ts
@@ -11,28 +11,43 @@ export class TagRepository {
         private readonly tagRepository: Repository<TagEntity>
     ) {}
 
+    /**
+     * 태그 이름을 담은 DTO를 받아와 새 태그를 생성하도록 한다.
+     */
     async create(createTagDTO: CreateTagDTO): Promise<TagEntity> {
         const tagEntity = this.tagRepository.create(createTagDTO);
 
         return this.tagRepository.save(tagEntity);
     }
 
+    /**
+     * 태그 이름을 받아와 일치하는 태그 정보를 리턴하도록 한다.
+     */
     async readByName(name: string): Promise<TagEntity> {
         return this.tagRepository.findOne({
             where: { name: name }
         });
     }
 
+    /**
+     * 태그 번호를 받아와 일치하는 태그 정보를 리턴하도록 한다.
+     */
     async readByTagNo(tag_no: number): Promise<TagEntity> {
         return this.tagRepository.findOne({
             where: { tag_no: tag_no }
         });
     }
 
+    /**
+     * 모든 태그 정보를 배열로 리턴하도록 한다.
+     */
     async list(): Promise<TagEntity[]> {
         return this.tagRepository.find();
     }
 
+    /**
+     * 태그 번호를 받아 해당 태그를 삭제하도록 한다.
+     */
     async delete(tag_no: number): Promise<object> {
         return this.tagRepository.delete({ tag_no });
     }
--- src/domain/tag/service/tag.service.ts
+++ src/domain/tag/service/tag.service.ts
@@ -7,6 +7,10 @@ import { TagEntity } from '../repository/tag.entity';
 export class TagService {
     constructor(private tagRepository: TagRepository) {}
 
+    /**
+     * 태그 이름을 담은 DTO를 받아와 중복되는 이름이 있는지 검사 후
+     * 정상적인 값이면 태그를 생성하도록 한다.
+     */
     async createTag(createTagDTO: CreateTagDTO): Promise<TagEntity> {
         const tagEntity = await this.tagRepository.readByName(createTagDTO.name);
 
@@ -17,10 +21,17 @@ export class TagService {
         return this.tagRepository.create(createTagDTO);
     }
 
+    /**
+     * 모든 태그를 불러오도록 한다.
+     */
     async listTag(): Promise<TagEntity[]> {
         return this.tagRepository.list();
     }
 
+    /**
+     * 태그 번호를 받아와 존재하는 태그인지 검사 후
+     * 정상적인 값이면 해당 태그를 삭제하도록 한다.
+     */
     async deleteTag(tag_no: number): Promise<object> {
         const tagEntity = await this.tagRepository.readByTagNo(tag_no);
 
