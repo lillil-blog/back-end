aa11bb58bf55e4f155bc5646ae74dc30871ea386
오성재 <monaco970808@gmail.com>
2024-04-05 13:11:23 +0900

    fix: ORM연결정보 환경변수 configModule로 변경

--- src/modules/typeorm.module.ts
+++ src/modules/typeorm.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule as NestTypeOrmModule } from '@nestjs/typeorm';
-import { BoardEntity } from 'src/domain/boards/repository/board.entity';
-import { BoardLikeEntity } from 'src/domain/boards/repository/board.like.entity';
-import { TagEntity } from 'src/domain/tags/repository/tag.entity';
-import { TagMappingEntity } from 'src/domain/tags/repository/tag.mapping.entity';
-import { UserEntity } from 'src/domain/users/repository/user.entity';
+import * as path from 'path';
 
 @Module({
     imports: [
-        NestTypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'osj-nas.synology.me',
-            port: 3306,
-            username: 'master',
-            password: 'Master3306!',
-            database: 'dstb',
-            entities: [UserEntity, BoardEntity, BoardLikeEntity, TagEntity, TagMappingEntity],
-            synchronize: true, // 재시작 시 테이블 구조 항상 동기화
-            logging: false // DB작업 콘솔에 로그 출력
+        NestTypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get('DB_HOST'),
+                port: configService.get('DB_PORT'),
+                username: configService.get('DB_USER'),
+                password: configService.get('DB_PASSWORD'),
+                database: configService.get('DB_DATABASE'),
+                entities: [path.join(__dirname, '../domain/**/repository/*.entity.{ts,js}')],
+                synchronize: true, // 재시작 시 테이블 구조 항상 동기화
+                logging: false // DB작업 콘솔에 로그 출력
+            })
         })
     ]
 })
