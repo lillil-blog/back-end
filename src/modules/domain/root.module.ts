ef0c15dbc540872650e9f235c9a77afc223c04f0
오성재 <2023a0031>
2024-04-12 15:34:48 +0900

    fix: module 수정

--- src/modules/domain/root.module.ts
+++ src/modules/domain/root.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardEntity } from 'src/domain/boards/repository/board.entity';
+import { BoardLikeEntity } from 'src/domain/boards/repository/board.like.entity';
 import { BoardRepository } from 'src/domain/boards/repository/board.repository';
 import { RootController } from 'src/domain/root/controller/root.controller';
 import { RootService } from 'src/domain/root/service/root.service';
@@ -9,7 +10,7 @@ import { UserEntity } from 'src/domain/users/repository/user.entity';
 import { UserRepository } from 'src/domain/users/repository/user.repository';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserEntity, BoardEntity, TagMappingEntity])],
+    imports: [TypeOrmModule.forFeature([UserEntity, BoardEntity, BoardLikeEntity, TagMappingEntity])],
     controllers: [RootController],
     providers: [RootService, UserRepository, BoardRepository]
 })
