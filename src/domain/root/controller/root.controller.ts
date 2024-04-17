f43a834791f1f5ceefe57ccfad5c3b36f50a8534
오성재 <monaco970808@gmail.com>
2024-04-17 17:03:37 +0900

    fix: Jenkins

--- src/domain/root/controller/root.controller.ts
+++ src/domain/root/controller/root.controller.ts
@@ -7,13 +7,13 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 export class RootController {
     constructor(private readonly rootService: RootService) {}
 
-    // @Get('/main')
-    // @ApiOperation({
-    //     summary: '메인페이지 Props',
-    //     description: '메인페이지에 뿌려줄 props 뭉치입니다. (게시글 최근4개, 유저정보들)'
-    // })
-    // @ApiResponse({ status: 200, description: '성공적으로 불러왔습니다.' })
-    // async getMainPageProps() {
-    //     return this.rootService.getMainPageProps(4);
-    // }
+    @Get('/main')
+    @ApiOperation({
+        summary: '메인페이지 Props',
+        description: '메인페이지에 뿌려줄 props 뭉치입니다. (게시글 최근4개, 유저정보들)'
+    })
+    @ApiResponse({ status: 200, description: '성공적으로 불러왔습니다.' })
+    async getMainPageProps() {
+        return this.rootService.getMainPageProps(4);
+    }
 }
